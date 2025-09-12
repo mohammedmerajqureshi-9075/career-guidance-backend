0e5cb6d2044e445b031dc5f9584f71d8ae604a2e
Abdul Raheman <abraheman.744@gmail.com>
2025-09-12 08:40:00 -0700

    addedexpressvalidator

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,16 +1,45 @@
 import express from "express";
-import { createBlog, deleteBlog, getAllBlogs, getBlogById, updateBlog } from "../controllers/blogControllers.js";
+import { body, param } from "express-validator";
+import { 
+  createBlog, 
+  deleteBlog, 
+  getAllBlogs, 
+  getBlogById, 
+  updateBlog 
+} from "../controllers/blogControllers.js";
+
 import { adminProtect, userAuthToken } from "../middlewares/authMiddleware.js";
-
-
+import { validate } from "../middlewares/validateMiddleware.js";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/create", adminProtect,createBlog);
-blogRouter.put("/update/:id",adminProtect, updateBlog);
-blogRouter.delete("/delete/:id",adminProtect,deleteBlog)
-blogRouter.get("/all",userAuthToken,getAllBlogs)
-blogRouter.get("/get/:id",userAuthToken,getBlogById)
+// Validation rules
+const createBlogValidation = [
+  body("title").isString().notEmpty().withMessage("Title is required and must be a string"),
+  body("content")
+    .isString()
+    .isLength({ min: 10 })
+    .withMessage("Content must be at least 10 characters long"),
+  body("author").optional().isString().withMessage("Author must be a string"),
+];
 
+const updateBlogValidation = [
+  body("title").optional().isString().withMessage("Title must be a string"),
+  body("content")
+    .optional()
+    .isString()
+    .isLength({ min: 10 })
+    .withMessage("Content must be at least 10 characters long"),
+  body("author").optional().isString().withMessage("Author must be a string"),
+];
+
+const idValidation = [param("id").isMongoId().withMessage("Invalid blog ID")];
+
+// Routes
+blogRouter.post("/create", adminProtect, createBlogValidation, validate, createBlog);
+blogRouter.put("/update/:id", adminProtect, idValidation.concat(updateBlogValidation), validate, updateBlog);
+blogRouter.delete("/delete/:id", adminProtect, idValidation, validate, deleteBlog);
+blogRouter.get("/all", userAuthToken, getAllBlogs);
+blogRouter.get("/get/:id", userAuthToken, idValidation, validate, getBlogById);
 
 export default blogRouter;
